15e6be38500ee08e631c73821da555f14be9ed57
Sungmin Park <C27Sungmin.Park@afacademy.af.edu>
2025-03-19 18:21:27 -0600

    lab 3

--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -1,3 +1,52 @@
+--+----------------------------------------------------------------------------
+--| 
+--| COPYRIGHT 2017 United States Air Force Academy All rights reserved.
+--| 
+--| United States Air Force Academy     __  _______ ___    _________ 
+--| Dept of Electrical &               / / / / ___//   |  / ____/   |
+--| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
+--| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
+--| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
+--| 
+--| ---------------------------------------------------------------------------
+--|
+--| FILENAME      : thunderbird_fsm_tb.vhd (TEST BENCH)
+--| AUTHOR(S)     : Capt Phillip Warner
+--| CREATED       : 03/2017
+--| DESCRIPTION   : This file tests the thunderbird_fsm modules.
+--|
+--|
+--+----------------------------------------------------------------------------
+--|
+--| REQUIRED FILES :
+--|
+--|    Libraries : ieee
+--|    Packages  : std_logic_1164, numeric_std
+--|    Files     : thunderbird_fsm_enumerated.vhd, thunderbird_fsm_binary.vhd, 
+--|				   or thunderbird_fsm_onehot.vhd
+--|
+--+----------------------------------------------------------------------------
+--|
+--| NAMING CONVENSIONS :
+--|
+--|    xb_<port name>           = off-chip bidirectional port ( _pads file )
+--|    xi_<port name>           = off-chip input port         ( _pads file )
+--|    xo_<port name>           = off-chip output port        ( _pads file )
+--|    b_<port name>            = on-chip bidirectional port
+--|    i_<port name>            = on-chip input port
+--|    o_<port name>            = on-chip output port
+--|    c_<signal name>          = combinatorial signal
+--|    f_<signal name>          = synchronous signal
+--|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
+--|    <signal name>_n          = active low signal
+--|    w_<signal name>          = top level wiring signal
+--|    g_<generic name>         = generic
+--|    k_<constant name>        = constant
+--|    v_<variable name>        = variable
+--|    sm_<state machine type>  = state machine type definition
+--|    s_<signal name>          = state name
+--|
+--+----------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -111,12 +160,16 @@ begin
     end process sim_proc;
     -----------------------------------------------------
 
-end test_bench;
+    -- Configuration block: Bind the UUT to its implementation.
+    configuration tb_cfg of thunderbird_fsm_tb is
+       for test_bench
+          for uut: thunderbird_fsm use entity work.thunderbird_fsm(thunderbird_fsm_arch);
+          end for;
+       end for;
+    end tb_cfg;
 
--- Configuration block to bind the component instance
-configuration tb_cfg of thunderbird_fsm_tb is
-   for test_bench
-      for uut: thunderbird_fsm use entity work.thunderbird_fsm(thunderbird_fsm_arch);
-      end for;
-   end for;
-end tb_cfg;
+end test_bench;
+	
+
+
+
